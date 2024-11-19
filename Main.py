4e4dc9388a22e0957e1469d4e917442979bdd93f
dkang <dkang24@icloud.com>
2024-11-19 10:26:05 -0500

    Quad Light ray scattering

--- Main.py
+++ Main.py
@@ -39,7 +39,12 @@ class Test(mglw.WindowConfig):
         white = LambertianMaterial(Texture(glm.vec3(.73, .73, .73)))
         green = LambertianMaterial(Texture(glm.vec3(.12, .45, .15)))
         light = PointLight(glm.vec3(15))
+        materialBack = ReflectiveMaterial(glm.vec3(0.8, 0.8, 0.8), 0)
+        materialBack = DielectricMaterial(1 / 1.3)
         earth = LambertianMaterial(Texture('Earth'))
+        materialGround = LambertianMaterial(Texture(glm.vec3(0.8, 0.8, 0.0)))
+        materialCenter = LambertianMaterial(Texture(glm.vec3(0.1, 0.2, 0.5)))
+        materialLeft = DielectricMaterial(1.0 / 1.3)
         
         self.world.addHittable(Quad(glm.vec3(555, 0, 0), glm.vec3(0, 555, 0), glm.vec3(0, 0, 555), green))
         self.world.addHittable(Quad(glm.vec3(0, 0, 0), glm.vec3(0, 555, 0), glm.vec3(0, 0, 555), red))
@@ -49,12 +54,6 @@ class Test(mglw.WindowConfig):
         self.world.addHittable(Quad(glm.vec3(0, 0, 555), glm.vec3(555, 0, 0), glm.vec3(0, 555, 0), white))
         self.world.addHittable(Cube(glm.vec3(130, 0, 65), glm.vec3(295, 165, 230), white))
         self.world.addHittable(Cube(glm.vec3(265, 0, 295), glm.vec3(430, 330, 460), white))
-
-        materialGround = LambertianMaterial(Texture(glm.vec3(0.8, 0.8, 0.0)))
-        materialCenter = LambertianMaterial(Texture(glm.vec3(0.1, 0.2, 0.5)))
-        materialLeft = DielectricMaterial(1.0 / 1.3)
-        materialRight = ReflectiveMaterial(glm.vec3(0.8, 0.6, 0.2), 0.5)
-        materialLight = PointLight(glm.vec3(2))
 
         self.world.addHittable(Sphere(glm.vec3(0, 0, -1), 0, materialCenter))
         
